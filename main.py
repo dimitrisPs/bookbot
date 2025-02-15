5d8d9d3b8a93795ae782db7f27ab6f5f53853c25
Dimitrios Psychogyios <d.psychogyios@gmail.com>
2025-02-15 22:09:16 +0000

    implement unique char count

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 def wc(txt):
     return len(txt.split())
     
+    
+def cc(txt):
+    occurances={}
+    for c in txt:
+        c = c.lower()
+        if c not in occurances:
+            occurances[c] = 0
+        occurances[c]+=1
+    return occurances
+        
 def main():
     with open('./books/frankenstein.txt') as f:
         txt =f.read()
     print(txt)
     
     print(wc(txt))
+    print(cc(txt))
     
 if __name__ == '__main__':
     main()
