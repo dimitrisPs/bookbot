e3c395f04255b366b2f0c93eb26b056a7ffb82d2
Dimitrios Psychogyios <d.psychogyios@gmail.com>
2025-02-15 22:19:15 +0000

    implement stats report

--- main.py
+++ main.py
@@ -10,14 +10,28 @@ def cc(txt):
             occurances[c] = 0
         occurances[c]+=1
     return occurances
+
+def filter_alphabetical(occurances):
+    a='c'
+    return {k:v for k,v in occurances.items() if k.isalpha()}
+
+def print_report(word_count, char_occ):
+    print('--- Begin report of books/frankenstein.txt ---')
+    print(f'{word_count} words found in the document\n')
+    
+    for c, times in char_occ.items():
+        print(f"The '{c}' character was found {times} times")
+    
+    print("--- End report ---")
         
 def main():
     with open('./books/frankenstein.txt') as f:
         txt =f.read()
-    print(txt)
     
-    print(wc(txt))
-    print(cc(txt))
+    word_count = wc(txt)
+    occ = cc(txt)
+    filt_occ = filter_alphabetical(occ)
+    print_report(word_count, filt_occ)
     
 if __name__ == '__main__':
     main()
