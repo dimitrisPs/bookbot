edeed89ee13ea46bb4aa527690002251b2ef2d4a
Dimitrios Psychogyios <d.psychogyios@gmail.com>
2025-02-15 22:03:36 +0000

    implement word count

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def wc(txt):
+    return len(txt.split())
+    
 def main():
     with open('./books/frankenstein.txt') as f:
         txt =f.read()
     print(txt)
+    
+    print(wc(txt))
     
 if __name__ == '__main__':
     main()
